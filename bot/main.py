2a162f0c8b60066bdb3c9ebb04c3ac69ecf8d6e0
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2024-01-03 18:09:19 +0000

    adding and sync command tree

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,3 @@
-
-from dotenv import dotenv_values
 import discord
 from discord.ext import commands
 import os
@@ -14,17 +12,22 @@ intent.guild_reactions = True
 intent.message_content = True
 # intent.commands = True
 
-bot = commands.Bot(command_prefix='!', intents=intent)
+bot = commands.Bot(command_prefix='/', intents=intent)
 
-@bot.command(name='greet')
-async def greet(ctx):
-    await ctx.send("Hello! I'm a bot. How can I assist you?")
+# tree = discord.app_commands.CommandTree(bot)
 
-@bot.command(name='mention')
-@commands.has_permissions(mention_everyone=True)
-async def tageveryone(ctx, *, message=None):
-    if not message:
-        message = "Attention everyone!"
-    await ctx.send(f"@everyone {ctx.author} msad3 lina k***a")
+# @bot.command(name='mention', description='Mention everyone in the server')
+@bot.tree.command(name='mention', description='Mention everyone in the server')
+#@commands.has_permissions(mention_everyone=True)
+async def mention(interaction: discord.Interaction):
+    username = interaction.user.name
+    role = discord.utils.get(interaction.guild.roles, name='FREAX')
+    await interaction.response.send_message(f"{role.mention} {username} msad3 lina k***a")
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f"Logged in as {bot.user}")
 
 bot.run(token)
+
