36e79a9052011a64d4857bcf84e7c9ec6d1093cf
Ucine Hamouni <62403196+UCINE@users.noreply.github.com>
2024-01-04 21:35:12 +0000

    fixed

--- bot/main.py
+++ bot/main.py
@@ -3,26 +3,34 @@ from discord.ext import commands
 import os
 
 token = os.environ.get('TOKEN')
-intent = discord.Intents.default()
-intent.members = True
-intent.messages = True
-intent.reactions = True
-intent.guild_messages = True
-intent.guild_reactions = True
-intent.message_content = True
 
-bot = commands.Bot(command_prefix='/', intents=intent)
+intents = discord.Intents.default()
+intents.members = True
+intents.messages = True
+intents.reactions = True
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.message_content = True
 
-@bot.tree.command(name='mention', description='Mention everyone in the server')
-async def mention(interaction: discord.Interaction):
-    username = interaction.user.name
-    role = discord.utils.get(interaction.guild.roles, name='FREAX')
-    await interaction.response.send_message(f"{role.mention} {username} msad3 lina k***a")
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+allowed_mentions = discord.AllowedMentions(roles=True)
+
+@bot.tree.command(name='mention', description='Mention a role in the server')
+async def mention(interaction: discord.Interaction, role_name: str = "freax"):
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+
+    if role:
+        if role.mentionable and interaction.channel.permissions_for(interaction.guild.me).mention_everyone:
+            await interaction.response.send_message(f"{role.mention} fri3 lk**r by {interaction.user.display_name}", allowed_mentions=allowed_mentions)
+            await interaction.response.send_message("had l9lawi khaso permissions.")
+    else:
+        await interaction.response.send_message("Role not found.")
 
 @bot.event
 async def on_ready():
     await bot.tree.sync()
     print(f"Logged in as {bot.user}")
 
-bot.run(token)
 
+bot.run(token)
